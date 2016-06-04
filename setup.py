11e78c2117aabc2c8a333952cf83a7da0f8de9ff
watcha.h <watcha.h@almacom.co.th>
2016-06-04 23:18:33 +0700

    init

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(
+    name="netforce_task",
+    version="3.1.0",
+    description="Task module",
+    author='Watcharapon Hongthong',
+)
